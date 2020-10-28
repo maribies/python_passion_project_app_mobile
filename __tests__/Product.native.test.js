81398299acf837073a361521eb4c89b61105c6ad
Marissa <marissa.biesecker@gmail.com>
2020-10-28 12:14:42 +0000

    Fix the theme passing in tests

--- __tests__/Product.native.test.js
+++ __tests__/Product.native.test.js
@@ -1,26 +1,42 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import { ThemeProvider } from "emotion-theming";
+
 import { Product } from "../Components/Product.native";
+import { theme } from "../theme";
+
 import products from "../Fixtures/products.json";
 
 const product = products[0];
 
 test("renders correctly with no data", () => {
-  const tree = create(<Product />).toJSON();
+  const tree = create(
+    <ThemeProvider theme={theme}>
+      <Product />
+    </ThemeProvider>
+  ).toJSON();
 
   expect(tree).toMatchSnapshot();
   expect.objectContaining({ Text: "Oops! No Data!" });
 });
 
 test("renders correctly with undefined data", () => {
-  const tree = create(<Product product={undefined} />).toJSON();
+  const tree = create(
+    <ThemeProvider theme={theme}>
+      <Product product={undefined} />
+    </ThemeProvider>
+  ).toJSON();
 
   expect(tree).toMatchSnapshot();
   expect.objectContaining({ Text: "Oops! No Data!" });
 });
 
 test("renders correctly with data", () => {
-  const tree = create(<Product product={product} />).toJSON();
+  const tree = create(
+    <ThemeProvider theme={theme}>
+      <Product product={product} />
+    </ThemeProvider>
+  ).toJSON();
 
   expect(tree).toMatchSnapshot();
   expect.objectContaining({ Text: "Chanel" });
---
+++ theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  margin: "20px",
+  fonts: {
+    xLarge: "33px",
+    large: "26px",
+    medium: "20px",
+    default: "16px",
+    small: "12.5px",
+    family: "Georgia",
+    weight: "normal",
+  },
+};
